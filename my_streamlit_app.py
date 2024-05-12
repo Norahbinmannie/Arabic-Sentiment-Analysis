4a3bbaa1ef5e44d34bf2c98c07f861e95985d380
Norahbinmannie <110065127+Norahbinmannie@users.noreply.github.com>
2024-05-12 14:24:26 +0300

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -3,19 +3,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 import joblib
 
-# تحميل النموذج المدرب والمتجه
+
 model = joblib.load("model.pkl")
 vectorizer = joblib.load("tfidf_vectorizer.pkl")
 
-# تحديد العناصر الواجهة
+
 st.title("Classifying sentiments ")
 text_input = st.text_input("Please enter text:")
 
-# التنبؤ بالتصنيف
+
 if text_input:
-    # تحويل النص إلى متجه TF-IDF
+
     text_vectorized = vectorizer.transform([text_input])
-    # التنبؤ باستخدام النموذج
+
     prediction = model.predict(text_vectorized)
     # عرض نتيجة التنبؤ
     if prediction == 1:
@@ -23,4 +23,4 @@ if text_input:
     elif prediction == -1:
         st.write("Negative: سلبي")
     else:
-        st.write("Neutral: طبيعي")
+        st.write("Neutral: محايد")
